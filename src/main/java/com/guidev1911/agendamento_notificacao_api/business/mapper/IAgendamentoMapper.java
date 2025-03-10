0f6b78d2f34a4d1c0fc088c8ea477e58abcc564c
guidev1911 <guilhermereal1911@gmail.com>
2025-03-09 23:09:24 -0300

    endpoint agendamento

--- src/main/java/com/guidev1911/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/guidev1911/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -3,7 +3,6 @@ package com.guidev1911.agendamento_notificacao_api.business.mapper;
 import com.guidev1911.agendamento_notificacao_api.controller.dto.in.AgendamentoRecord;
 import com.guidev1911.agendamento_notificacao_api.controller.dto.out.AgendamentoRecordOut;
 import com.guidev1911.agendamento_notificacao_api.infrastructure.entities.Agendamento;
-import com.guidev1911.agendamento_notificacao_api.infrastructure.repository.AgendamentoRepository;
 import org.mapstruct.Mapper;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
