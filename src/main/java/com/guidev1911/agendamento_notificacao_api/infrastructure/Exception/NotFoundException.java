ad6e3af1bef4a6fb8af65237b7451d947798c760
guidev1911 <guilhermereal1911@gmail.com>
2025-03-11 23:45:55 -0300

    exception

---
+++ src/main/java/com/guidev1911/agendamento_notificacao_api/infrastructure/Exception/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.guidev1911.agendamento_notificacao_api.infrastructure.Exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) { super(message);}
+}
